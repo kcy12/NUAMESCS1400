a1662cfba823fb1a90e5e2061a80f123f713346e
ethanpickard <33161628+ethanpickard@users.noreply.github.com>
2018-04-30 13:01:25 -0600

    yes

--- 7/Pickard_E/Caesar Cipher/student code/Caesar.java
+++ 7/Pickard_E/Caesar Cipher/student code/Caesar.java
@@ -42,13 +42,10 @@ public class Caesar{
             }
          }
          
-         else {
-            //add the cipher to the int
-				//if the new number is out the range of capital letters, wrap it back, ie. z+1=a, z+2=b
-         }
-			//add the char represented by the int to the encrypted message string  
+			  
          
          subChar = (char) charInt;
+         //add the char represented by the int to the encrypted message string
          encryptMessage.appends(subChar);      
       }
 
@@ -57,22 +54,37 @@ public class Caesar{
    
    public static int[] letterDistr(String message) {
 		//initialize an int array to hold the number of each letter
+      int[] messageArray = new int[26];
 		
       //for each character in the message
+      for (int i = 0; i < message.length(); i++) {
+         char currentChar = message.CharAt(i);
+         int charInt = (int) subChar;
          //convert the current char to lowercase then to an int
+         if (charInt > 64 && charInt < 91) {
+            charInt = charInt + 33;
+         }
          //if the int represents a lowercase letter (check the ascii chart)
+         if (charInt > 96 && charInt < 123) {
             //subtract 97 from the int
+            charInt = charInt - 97;
             //add 1 to the int array at that index
-         //return the int array
+            messageArray[charInt] += 1;
+         }
+      }//end for
+      return messageArray;
 	}//end letter distribution method
    
 	public static void printDistr(int[] distr) {
       //for each element in the array
-         //create a char from the current position (ie. 0=a,1=b,...)
-			System.out.print(/*letter*/ + ": " + distr[i] + "|");
-         //for the number in the array at the current position
-            //print an * on the same line
-			//move to the next line
+      for (int i; i < distr.length; i++) {
+         char c = (char) i + 97; //create a char from the current position (ie. 0=a,1=b,...)
+         System.out.print(c + ": " + distr[i] + "|");
+         for (int j = 0; j < distr[i]; j++) { //for the number in the array at the current position
+            System.out.print("*");
+            System.out.println(); //move to the next line
+         }
+      }		
 	}//end printDistr
    
 }//end class Caersar
